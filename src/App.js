43e735bfaf0ce721fb608415a2d2b207e54e0ac2
vrajesh0167 <131984637+vrajesh0167@users.noreply.github.com>
2023-10-05 14:42:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import UseredComponenrs from './Components/UseredComponenrs';
-import A from './Components/A';
+import "./App.css";
+import UseredComponenrs from "./Components/UseredComponenrs";
+import A from "./Components/A";
 
 function App() {
+
   return (
-    <div className=' container mt-5'>
+    <div className=" container mt-5">
       <UseredComponenrs></UseredComponenrs>
 
-      <div className=' mt-5'>
+      <div className=" mt-5">
         <A></A>
       </div>
     </div>
